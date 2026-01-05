ba3958f116d25ce00ffdd439f33950ebb8906d65
Tanish Dasari <110691492+TD2009@users.noreply.github.com>
2026-01-05 12:27:16 -0600

    Update and rename bovineShuffle.java to BovineShuffle.java

--- Bronze/BovineShuffle.java
+++ Bronze/BovineShuffle.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.StringTokenizer;
 
-public class bovineShuffle {
+public class BovineShuffle {
     public static void main(String[] args) throws IOException{
         BufferedReader qwerty = new BufferedReader(new FileReader("shuffle.in"));
         StringTokenizer st = new StringTokenizer(qwerty.readLine());
