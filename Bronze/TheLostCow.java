5743a24dce91c2ed70ae6bd19d4e1dbc5653fe70
Tanish Dasari <110691492+TD2009@users.noreply.github.com>
2026-01-05 12:26:55 -0600

    Update and rename theLostCow.java to TheLostCow.java

--- Bronze/TheLostCow.java
+++ Bronze/TheLostCow.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.StringTokenizer;
 
-public class theLostCow {
+public class TheLostCow {
     public static void main(String[] args) throws IOException{
         BufferedReader qwerty = new BufferedReader(new FileReader("lostcow.in"));
         StringTokenizer st = new StringTokenizer(qwerty.readLine());
